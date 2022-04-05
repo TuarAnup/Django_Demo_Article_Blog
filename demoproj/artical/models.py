f20031f7151d812571d8b81e3ed8fa08ac62ea91
TuarAnup <anupe.raut81@gmail.com>
2022-04-05 13:17:31 +0545

    Buttons revised

--- demoproj/artical/models.py
+++ demoproj/artical/models.py
@@ -5,7 +5,7 @@ from matplotlib.pyplot import title
 
 class Article(models.Model):
     title = models.CharField(max_length= 255)
-    content = models.CharField(max_length = 1000)
+    content = models.CharField(max_length = 1000000)
     date_published = models.DateField()
     date_verified = models.DateField(null = True)
     #email = models.EmailField(null=True)
